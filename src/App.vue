a753e375152e7ea5e6e0d911c4c98c0a22abbd70
danielmlc <danielmlc@126.com>
2022-03-28 09:52:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+   测试
+  <div id="app">
+    aaaa
+    <el-button type="primary">Primary Button</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+#app {
+  color: #2c3e50;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  transform: rotate(0deg);
+}
+</style>
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as Vue from 'vue'
+import App from './App'
+
+const app = Vue.createApp(App)
+// eslint-disable-next-line no-undef
+app.use(ElementPlus)
+app.mount('#app')
